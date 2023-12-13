2819e91dd15a9f538321811db16eaac836f795ce
Carlos Méndez <cmendez@doyoukare.com>
2023-12-12 19:29:48 -0600

    Fix error handling

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const mimeTypesAllowed = [
 
 const upload = multer({
   dest: 'uploads/',
-  limits: { fieldSize: 25 * 1024 * 1024 }, // 25MB
+  limits: { fileSize: 25 * 1024 * 1024 }, // 25MB
   fileFilter: async (req, file, cb) => {
     console.log(file.mimetype)
     console.log(!mimeTypesAllowed.includes(file.mimetype))
@@ -20,7 +20,6 @@ const upload = multer({
       return cb(new Error('File type not allowed', false))
     }
     cb(null, true)
-    // cb(new Error('File type not allowed', false))
   },
 })
 const port = 3000
@@ -35,13 +34,24 @@ app.post('/', (req, res) => {
   res.send('Got a POST request')
 })
 
-app.post('/send', upload.single('file'), (req, res) => {
-  const formData = req.file
-  const bodyData = req.body
-  console.log('form data', formData)
-  console.log('bodyData', bodyData)
-  res.send({ success: true })
-})
+app.post(
+  '/send',
+  upload.single('file'),
+  (req, res) => {
+    const formData = req.file
+    const bodyData = req.body
+    console.log('form data', formData)
+    console.log('bodyData', bodyData)
+    res.send({ success: true })
+  },
+  (error, req, res, next) => {
+    if (error) {
+      res.status(415).json({ success: false, message: error.message })
+    } else {
+      next()
+    }
+  }
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
